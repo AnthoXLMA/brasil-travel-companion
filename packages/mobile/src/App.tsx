9221ea0d0dc89c78e3cae065433a4709c2abf9ac
Anthony Mania <amj2544@tutanota.com>
2025-08-10 08:44:14 +0200

    lancement 2

---
+++ packages/mobile/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Bonjour Brasil Travel Companion !</Text>
+    </View>
+  );
+}
+
